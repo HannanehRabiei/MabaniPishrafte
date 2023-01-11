f1ed72987e75ceaf52f3934c19dcecf274765662
Hannaneh Rabiei <hannanehrabiei82@gmail.com>
2023-01-11 19:33:21 +0330

    foroshande

---
+++ MabaniPishrateJava/src/main/java/Khodro/Khodro.java
@@ -0,0 +1,14 @@
+package Khodro;
+
+public abstract class Khodro {
+
+    protected void protectedMethod(){
+        System.out.println("this method is protected");
+    }
+    public abstract String typedande();
+
+    public abstract int gonjayeshmashin();
+
+    public abstract String getmark();
+}
+
---
+++ MabaniPishrateJava/src/main/java/froshande/Froshande.java
@@ -0,0 +1,9 @@
+package froshande;
+
+import Khodro.New.Mashin;
+
+public class Froshande {
+    public void foroshandeMashin() {
+        Mashin.tedadeforosh++;
+    }
+}
