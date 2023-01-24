314ad71aed45755a74e916aa27b2c1972056a5eb
alijavaboy <ali77erfagh99@gmail.com>
2023-01-24 22:03:16 +0330

    fix bug plus

--- Tamrin shey gara/src/test/java/MathTest.java
+++ Tamrin shey gara/src/test/java/MathTest.java
@@ -1,13 +1,13 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
-public class MathTest {
-    @Test
-    void Bayad_Amaliat_Riazi_Anjam_Dahad() {
-        double motaghayer = -1.5;
-        double meghdarMotlagh = Math.abs(motaghayer);  // abs : static method
-        Assertions.assertEquals(1.5 , meghdarMotlagh);
-
-        Assertions.assertEquals(1 , Math.cos(0));
-    }
-}
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.Test;
+//
+//public class MathTest {
+//    @Test
+//    void Bayad_Amaliat_Riazi_Anjam_Dahad() {
+//        double motaghayer = -1.5;
+//        double meghdarMotlagh = Math.abs(motaghayer);  // abs : static method
+//        Assertions.assertEquals(1.5 , meghdarMotlagh);
+//
+//        Assertions.assertEquals(1 , Math.cos(0));
+//    }
+//}
